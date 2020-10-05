8072e23955925cf8d173f9ebc74af86dc02aa9be
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-05 22:48:52 +0530

    using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -30,4 +30,23 @@ else
 	empHrs=0;
 fi
 
-salary =$(( $empHrs*$empRatePerHr ));
+salary=$(( $empHrs*$empRatePerHr ));
+
+##part time wage using switch case
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3))
+
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+salary=$(($empHrs*$empRatePerHr));
