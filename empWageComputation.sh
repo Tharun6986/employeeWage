5ed5f038932076ea051abe9bb649941316423e5f
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-06 01:48:02 +0530

    Total Working Hours using Function is done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -118,3 +118,37 @@ do
         totalEmpHrs=$(($totalEmpHrs+$empHrs))
         totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
 done
+
+##Refactoring the above code using function
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+function getWorkingHours() {
+
+         case $1 in
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        echo $empHrs
+}
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        totalWorkingDays=$(($totalWorkingDays+1))
+        empHrs="$( getWorkingHours $((RANDOM%3)) )"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
+done
