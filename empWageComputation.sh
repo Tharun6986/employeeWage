6f26e21d809c738108e6d30f8032260ef57da299
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-05 17:57:41 +0530

    welcome

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,5 @@
-#!/bin/bash -x
 
-isPresent=1;
-randomCheck=$(( RANDOM%2 ));
+echo "welcome to employee wage calculation"
 
-if [ $isFullTime -eq $randomCheck ]
-then
-        echo"Employee is present"
-else
-        echo "Employee is abscent"
-fi
+
+
