fb6453f108065437fe8b1ce4870a82131e35ef8e
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-06 01:41:43 +0530

    wages calculated for total working hours or days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -89,3 +89,32 @@ then
 else
         salary=0;
 fi
+
+##code for total working hours or days for a month
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        totalWorkingDays=$(($totalWorkingDays+1))
+        empCheck=$((RANDOM%3))
+        case $empCheck in
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
+done
