74b9c6b24063278f778b276e493fc3a16604cfed
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-05 20:35:54 +0530

    part time wage added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -14,3 +14,20 @@ else
         echo "Employee is abscent"
 fi
 
+##code for part time wage
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if [ $isFullTime -eq $randomCheck ];
+then
+	empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+	empHrs=4;
+else
+	empHrs=0;
+fi
+
+salary =$(( $empHrs*$empRatePerHr ));
